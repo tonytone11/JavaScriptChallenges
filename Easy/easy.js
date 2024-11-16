0394fc17b1e82fb61458b3e209948eb5e78846b6
Anthony Montesdeoca <anthony_chen97@yahoo.com>
2024-11-15 23:41:45 -0500

    js code for Easy

---
+++ Easy/easy.js
@@ -0,0 +1,7 @@
+function excerciseOfDay(name) {
+    console.log("Today's exercise: " + name);
+}
+excerciseOfDay("Running");
+excerciseOfDay("Swimming");
+excerciseOfDay("Dancing");
+excerciseOfDay("Fencing");
